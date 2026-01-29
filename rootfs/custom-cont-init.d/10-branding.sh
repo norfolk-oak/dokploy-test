c21c3c0c1ef96ea41c77be7bead88566bf5fef8c
LSmithx2 <42276854+lsmithx2@users.noreply.github.com>
2026-01-29 17:38:45 +0000

    Delete rootfs directory

--- rootfs/custom-cont-init.d/10-branding.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Overwrite the existing LibreSpeed CSS file
-cp -f /opt/naked-style.css /config/www/style.css
-
-# Best-effort permissions
-chown abc:abc /config/www/style.css 2>/dev/null || true
